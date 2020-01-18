7b2d87fb24e6d62212bbb22b1a5921aa15016c60
Mat Sz <contact@matsz.dev>
2020-01-18 21:01:07 +0100

    added copy to clipboard

--- src/components/Output/index.tsx
+++ src/components/Output/index.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import CopyToClipboard from 'react-copy-to-clipboard';
 import styles from './styles.module.scss';
 
 const Output: React.FC<{ command: string }> = ({ command }) => {
+    const [ copied, setCopied ] = useState(false);
+
+    useEffect(() => {
+        // Reset the button text when command changes.
+        setCopied(false);
+    }, [ command, setCopied ]);
+
+    const onCopy = useCallback(() => setCopied(true), [ setCopied ]);
+
     return (
         <div className={styles.output}>
             <pre>
                 <span>$&nbsp;</span>{ command }
             </pre>
-            <button>Copy to clipboard</button>
+            <CopyToClipboard
+                text={command}
+                onCopy={onCopy}>
+                <button>
+                    { copied ? 'Copied' : 'Copy to clipboard' }
+                </button>
+            </CopyToClipboard>
         </div>
     );
 }
